41d18294f0477dbb73f10b203108984248fb1405
blade-01 <animashauntaofiq@yahoo.com>
2021-08-18 16:14:56 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,69 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <sign-up />
+  <div class="container">
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SignUp from './components/signUp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SignUp
   }
 }
 </script>
 
 <style>
-#app {
+body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-size: 15px;
+}
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.box-shadow {
+    -webkit-box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.12), 0 1px 5px 0 rgba(0, 0, 0, 0.2);
+    box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.12), 0 1px 5px 0 rgba(0, 0, 0, 0.2);
+}
+.btn-shadow {
+    -webkit-box-shadow: 0 3px 3px 0 rgba(0, 0, 0, 0.14), 0 1px 7px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -1px rgba(0, 0, 0, 0.2);
+    box-shadow: 0 3px 3px 0 rgba(0, 0, 0, 0.14), 0 1px 7px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -1px rgba(0, 0, 0, 0.2);
+}
+.container {
+  width: 90%;
+  margin: auto;
+  overflow: hidden;
+}
+a {
+  text-decoration: none;
+}
+input {
+  outline: none;
+  border: solid 1px gray;
+  padding: 0.8rem 1rem;
+  border-radius: 5px;
+}
+@media screen and (min-width: 700px) {
+  .container {
+    width: 80%;
+  }
+}
+@media screen and (min-width: 1000px) {
+  .container {
+    width: 968px;
+  }
+}
+@media screen and (min-width: 1200px) {
+  .container {
+    width: 1100px;
+  }
 }
 </style>
---
+++ src/components/signUp.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="wrapper">
+    <div class="bg-line"></div>
+    <div class="sign-up">
+      <div class="container">
+        <h1>stripe</h1>
+        <form class="box-shadow">
+          <h3>Sign in to your account</h3>
+          <div class="input-field">
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" placeholder="jane.doe@gmail.com">
+          </div>
+          <div class="input-field">
+            <div class="pass-flex">
+              <label for="password">Password</label>
+              <a href="#">Forgot your password</a>
+            </div>
+            <input type="password" name="password" id="password" placeholder="Password">
+          </div>
+          <div class="check-field">
+            <input type="checkbox" name="signed" id="signed">
+            <label for="signed">Stay signed in for a week</label>
+          </div>
+          <div class="submit">
+            <input type="submit" value="Continue" class="btn">
+            <a href="#">Use single sign-on (SSO) instead</a>
+          </div>
+        </form>
+        <div class="create-acct">
+          <p>Don't have an account? <a href="#">Sign up</a></p>
+          <div class="pri-pol">
+            <p>&copy; Stripe</p>
+            <p>Contact</p>
+            <p>Privacy & terms</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.sign-up form {
+  padding: 2rem 1rem;
+  margin: 2rem 0.2rem;
+}
+.sign-up form .input-field {
+  margin: 2rem 0 0;
+}
+.sign-up form .input-field input {
+  display: block;
+  margin: 1rem 0;
+  width: 100%;
+}
+.sign-up form .input-field .pass-flex {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.sign-up form .check-field {
+  margin: 2rem 0;
+}
+.sign-up form .check-field label {
+  margin-left: 0.5rem;
+}
+.sign-up form .submit {
+  text-align: center;
+}
+.sign-up form .btn {
+  display: block;
+  width: 100%;
+  font-weight: bold;
+  margin: 0 0 2rem;
+}
+.sign-up .create-acct p {
+  margin: 0 0 1rem;
+}
+.sign-up .pri-pol {
+  display: flex;
+  gap: 0.5rem;
+}
+@media screen and (min-width: 700px) {
+  .wrapper {
+    position: relative;
+  }
+  .sign-up {
+    background: rgba(0, 0, 0, 0.2);
+    width: 700px;
+    margin: auto;
+    z-index: 10;
+    position: relative;
+  }
+  .sign-up form {
+    padding: 3rem;
+    background: #f4f4f4;
+  }
+  .sign-up .container {
+    max-width: 500px;
+  }
+  .bg-line {
+    position: absolute;
+    top: 50%;
+    left: 0%;
+    /* transform: translate(0%, -50%); */
+    transform: rotate(-15deg);
+    width: 100%;
+    height: 20px;
+    background: blue;
+  }
+}
+</style>
